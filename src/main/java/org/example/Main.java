0066300f6d621af85a47482fc321aeaf5bb9c4d8
Vladislav Bobovoz <vladislav.bobovoz@outlook.com>
2023-01-15 15:44:28 +0300

    TXT tests

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,39 +5,5 @@ import org.example.ConsoleUI.ConsoleUI;
 public class Main{
     public static void main(String[] args) throws Exception {
         ConsoleUI.consoleUI();
-
-        //Encryption.encrypt("mysecretkeyforcc", "work/input.txt", "work/einput.txt");
-
-//        ArrayList<ArrayList<String>> txtData = new ArrayList<>(TXT.ReadTXT("inputFiles/input.txt"));
-//        TXT.WriteResultTXT(txtData, "outputFiles/output.txt");
-//
-//        ArrayList<ArrayList<String>> jsonData = new ArrayList<>(JSON.ReadJSON("inputFiles/input.json"));
-//        JSON.WriteResultJSON(jsonData, "outputFiles/output.json");
-//
-//        ArrayList<ArrayList<String>> xmlData = new ArrayList<>(XML.ReadXML("inputFiles/input.xml"));
-//        XML.WriteResultXML(xmlData, "outputFiles/output.xml");
-//
-//        List<String> filePaths = Arrays.asList("inputFiles/input.json", "inputFiles/input.xml", "inputFiles/input.txt");
-//        Archiving.createArchive(filePaths, "outputFiles/Files.zip");
-//
-//        Archiving.extractArchive("outputFiles/Files.zip", "extractArchiveFiles");
-//
-//        Compressing.compressFile("inputFiles/input.json", 9);
-//        Compressing.decompressFile("inputFiles/input.json.zip");
-//
-        //System.out.println(Archiving.isArchived("inputFiles/input.txt"));
-        //System.out.println(Archiving.isArchived("outputFiles/Files.zip"));
-
-//        List<String> filePaths = Arrays.asList("inputFiles/input.txt", "inputFiles/input.json", "inputFiles/input.xml");
-//        ArchivingJAR.createArchiveJAR(filePaths, "outputFiles/Files.jar");
-//        ArchivingJAR.extractArchiveJAR("outputFiles/Files.jar", "extractArchiveFiles");
-//        System.out.println(ArchivingJAR.isArchivedJAR("inputFiles/input.txt"));
-//        System.out.println(ArchivingJAR.isArchivedJAR("outputFiles/Files.jar"));
-
-        //Encryption.encrypt("mysecretkeyforcc", "encryptFiles/DecryptFiles.zip", "encryptFiles/EncryptFiles.zip");
-
-//        Encryption.encrypt("mysecretkeyforcc", "outputFiles/Files.zip", "encryptFiles/EncryptFiles.zip");
-//        Encryption.decrypt("mysecretkeyforcc", "encryptFiles/encrypted.txt", "encryptFiles/decrypted.txt");
-//        Encryption.decrypt("mysecretkeyforcc", "encryptFiles/EncryptFiles.zip", "encryptFiles/DecryptFiles.zip");
     }
 }
---
+++ src/test/java/Tests/TESTS_FOR_TXT.java
@@ -0,0 +1,76 @@
+package Tests;
+
+import org.example.ReadingAndWriting.TXT;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static junit.framework.TestCase.*;
+
+public class TESTS_FOR_TXT {
+    @Test
+    public void testReadTXT() throws IOException {
+        File file = File.createTempFile("test", ".txt");
+        FileWriter writer = new FileWriter(file);
+        writer.write("Line 1\nLine 2\nLine 3");
+        writer.close();
+
+        ArrayList<ArrayList<String>> expected = new ArrayList<>();
+        ArrayList<String> line1 = new ArrayList<>();
+        line1.add("Line 1");
+        ArrayList<String> line2 = new ArrayList<>();
+        line2.add("Line 2");
+        ArrayList<String> line3 = new ArrayList<>();
+        line3.add("Line 3");
+        expected.add(line1);
+        expected.add(line2);
+        expected.add(line3);
+
+        ArrayList<ArrayList<String>> result = TXT.ReadTXT(file.getAbsolutePath());
+        assertNotNull(result);
+        assertEquals(expected, result);
+
+        file.delete();
+    }
+
+    @Test
+    public void ReadTXT_FileNotFound() {
+        ArrayList<ArrayList<String>> result = TXT.ReadTXT("non-existing-file.txt");
+        assertNull(result);
+    }
+
+    @Test
+    public void WriteTXT_test() throws IOException {
+        File file = File.createTempFile("test", ".txt");
+        ArrayList<ArrayList<String>> lines = new ArrayList<>();
+        ArrayList<String> line1 = new ArrayList<>();
+        line1.add("Line 1");
+        ArrayList<String> line2 = new ArrayList<>();
+        line2.add("Line 2");
+        ArrayList<String> line3 = new ArrayList<>();
+        line3.add("Line 3");
+        lines.add(line1);
+        lines.add(line2);
+        lines.add(line3);
+
+        TXT.WriteTXT(lines, file.getAbsolutePath());
+
+        Scanner reader = new Scanner(new FileReader(file));
+        StringBuilder builder = new StringBuilder();
+        while(reader.hasNextLine()) {
+            builder.append(reader.nextLine());
+            if(reader.hasNextLine()) {
+                builder.append("\n");
+            }
+        }
+        reader.close();
+        assertEquals("Line 1\nLine 2\nLine 3", builder.toString());
+
+        file.delete();
+    }
+}
